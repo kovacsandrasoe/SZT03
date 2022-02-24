8b007d4207e46ca7dd778b31d34c40424cbd5169
kovacs.andras <kovacs.andras@nik.uni-obuda.hu>
2022-02-24 23:25:23 +0100

    person added

---
+++ SZT03/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZT03
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
